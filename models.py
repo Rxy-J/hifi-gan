d7cdca020281a17ca2f7334b9c104dc2a5833a8f
OrangeJ <mkj66541@gmail.com>
2022-11-09 12:21:06 +0800

    add pqmf into generator

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+import numpy as np
 from torch.nn import Conv1d, ConvTranspose1d, AvgPool1d, Conv2d
 from torch.nn.utils import weight_norm, remove_weight_norm, spectral_norm
 from utils import init_weights, get_padding
+from scipy.signal import kaiser
 
 LRELU_SLOPE = 0.1
 
@@ -77,7 +79,9 @@ class Generator(torch.nn.Module):
         super(Generator, self).__init__()
         self.h = h
         self.num_kernels = len(h.resblock_kernel_sizes)
+        self.upsample_rates = h.upsample_rates
         self.num_upsamples = len(h.upsample_rates)
+        self.use_sub_band = True  if h.get("num_band") and h.num_bands > 1 else False
         self.conv_pre = weight_norm(Conv1d(80, h.upsample_initial_channel, 7, 1, padding=3))
         resblock = ResBlock1 if h.resblock == '1' else ResBlock2
 
@@ -93,15 +97,30 @@ class Generator(torch.nn.Module):
             for j, (k, d) in enumerate(zip(h.resblock_kernel_sizes, h.resblock_dilation_sizes)):
                 self.resblocks.append(resblock(h, ch, k, d))
 
-        self.conv_post = weight_norm(Conv1d(ch, 1, 7, 1, padding=3))
+        decoder_out_channel = h.num_bands if self.use_sub_band else 1
+        self.conv_post = weight_norm(Conv1d(ch, decoder_out_channel, 7, 1, padding=3))
         self.ups.apply(init_weights)
         self.conv_post.apply(init_weights)
+
+        if self.use_sub_band:
+            self.pqmf = PQMF(h.num_bands)
+            self.post_filter = weight_norm(Conv1d(
+                in_channels=1,
+                out_channels=1,
+                kernel_size=h.post_channel[0],
+                stride=h.post_channel[1],
+                padding=(h.post_channel[0]-1)//2
+            ))
+            self.pqmf.apply(init_weights)
+            self.post_filter.apply(init_weights)
 
     def forward(self, x):
         x = self.conv_pre(x)
         for i in range(self.num_upsamples):
             x = F.leaky_relu(x, LRELU_SLOPE)
+            before_length = x.size(-1)
             x = self.ups[i](x)
+            x = x[:, :, :before_length*self.upsample_rates[i]]
             xs = None
             for j in range(self.num_kernels):
                 if xs is None:
@@ -113,6 +132,9 @@ class Generator(torch.nn.Module):
         x = self.conv_post(x)
         x = torch.tanh(x)
 
+        if self.use_sub_band:
+            x = self.pqmf.synthesis(x)
+            x = self.post_filter(x)
         return x
 
     def remove_weight_norm(self):
@@ -123,6 +145,7 @@ class Generator(torch.nn.Module):
             l.remove_weight_norm()
         remove_weight_norm(self.conv_pre)
         remove_weight_norm(self.conv_post)
+        remove_weight_norm(self.post_filter)
 
 
 class DiscriminatorP(torch.nn.Module):
@@ -280,4 +303,211 @@ def generator_loss(disc_outputs):
         loss += l
 
     return loss, gen_losses
+
+def design_prototype_filter(taps=62, cutoff_ratio=0.142, beta=9.0):
+    """Design prototype filter for PQMF.
+    This method is based on `A Kaiser window approach for the design of prototype
+    filters of cosine modulated filterbanks`_.
+    Args:
+        taps (int): The number of filter taps.
+        cutoff_ratio (float): Cut-off frequency ratio.
+        beta (float): Beta coefficient for kaiser window.
+    Returns:
+        ndarray: Impluse response of prototype filter (taps + 1,).
+    .. _`A Kaiser window approach for the design of prototype filters of cosine modulated filterbanks`:
+        https://ieeexplore.ieee.org/abstract/document/681427
+    """
+    # check the arguments are valid
+    assert taps % 2 == 0, "The number of taps mush be even number."
+    assert 0.0 < cutoff_ratio < 1.0, "Cutoff ratio must be > 0.0 and < 1.0."
+
+    # make initial filter
+    omega_c = np.pi * cutoff_ratio
+    with np.errstate(invalid='ignore'):
+        h_i = np.sin(omega_c * (np.arange(taps + 1) - 0.5 * taps)) \
+            / (np.pi * (np.arange(taps + 1) - 0.5 * taps))
+    h_i[taps //
+        2] = np.cos(0) * cutoff_ratio  # fix nan due to indeterminate form
+
+    # apply kaiser window
+    w = kaiser(taps + 1, beta)
+    h = h_i * w
+
+    return h
+
+class PQMF(torch.nn.Module):
+    """PQMF module.
+    This module is based on `Near-perfect-reconstruction pseudo-QMF banks`_.
+    .. _`Near-perfect-reconstruction pseudo-QMF banks`:
+        https://ieeexplore.ieee.org/document/258122
+    """
+
+    def __init__(self, subbands=4, taps=62, cutoff_ratio=0.142, beta=9.0):
+        """Initilize PQMF module.
+        The cutoff_ratio and beta parameters are optimized for #subbands = 4.
+        See dicussion in https://github.com/kan-bayashi/ParallelWaveGAN/issues/195.
+        Args:
+            subbands (int): The number of subbands.
+            taps (int): The number of filter taps.
+            cutoff_ratio (float): Cut-off frequency ratio.
+            beta (float): Beta coefficient for kaiser window.
+        """
+        super(PQMF, self).__init__()
+
+        if subbands == 8:
+            cutoff_ratio = 0.07949452
+        elif subbands == 6:
+            cutoff_ratio = 0.10032791
+        # build analysis & synthesis filter coefficients
+        h_proto = design_prototype_filter(taps, cutoff_ratio, beta)
+        h_analysis = np.zeros((subbands, len(h_proto)))
+        h_synthesis = np.zeros((subbands, len(h_proto)))
+        for k in range(subbands):
+            h_analysis[k] = 2 * h_proto * np.cos((2 * k + 1) *
+                                                 (np.pi / (2 * subbands)) *
+                                                 (np.arange(taps + 1) -
+                                                  (taps / 2)) +
+                                                 (-1)**k * np.pi / 4)
+            h_synthesis[k] = 2 * h_proto * np.cos((2 * k + 1) *
+                                                  (np.pi / (2 * subbands)) *
+                                                  (np.arange(taps + 1) -
+                                                   (taps / 2)) -
+                                                  (-1)**k * np.pi / 4)
+
+        # convert to tensor
+        analysis_filter = torch.from_numpy(h_analysis).float().unsqueeze(1)
+        synthesis_filter = torch.from_numpy(h_synthesis).float().unsqueeze(0)
+
+        # register coefficients as beffer
+        self.register_buffer("analysis_filter", analysis_filter)
+        self.register_buffer("synthesis_filter", synthesis_filter)
+
+        # filter for downsampling & upsampling
+        updown_filter = torch.zeros((subbands, subbands, subbands)).float()
+        for k in range(subbands):
+            updown_filter[k, k, 0] = 1.0
+        self.register_buffer("updown_filter", updown_filter)
+        self.subbands = subbands
+
+        # keep padding info
+        self.pad_fn = torch.nn.ConstantPad1d(taps // 2, 0.0)
+
+    def analysis(self, x):
+        """Analysis with PQMF.
+        Args:
+            x (Tensor): Input tensor (B, 1, T).
+        Returns:
+            Tensor: Output tensor (B, subbands, T // subbands).
+        """
+        x = F.conv1d(self.pad_fn(x), self.analysis_filter)
+        return F.conv1d(x, self.updown_filter, stride=self.subbands)
+
+    def synthesis(self, x):
+        """Synthesis with PQMF.
+        Args:
+            x (Tensor): Input tensor (B, subbands, T // subbands).
+        Returns:
+            Tensor: Output tensor (B, 1, T).
+        """
+        # NOTE(kan-bayashi): Power will be dreased so here multipy by # subbands.
+        #   Not sure this is the correct way, it is better to check again.
+        # TODO(kan-bayashi): Understand the reconstruction procedure
+        x = F.conv_transpose1d(x,
+                               self.updown_filter * self.subbands,
+                               stride=self.subbands)
+        return F.conv1d(self.pad_fn(x), self.synthesis_filter)
+
+
+class AudioEncoder(nn.Module):
+    def __init__(self, audio_encoder_layers, audio_encoder_pool, vae_dim):
+        super().__init__()
+        
+        self.encoders = nn.ModuleList()
+
+        for i in range(audio_encoder_layers):
+            self.encoders.append(EncoderNet())
+
+        self.pools = nn.ModuleList()
+        for i in range(audio_encoder_layers - 1):
+            self.pools.append(
+                AvgPool1d(
+                    kernel_size = audio_encoder_pool[0],
+                    stride=audio_encoder_pool[1])
+                    )
+                
+        self.mean_linear = nn.Linear(48, vae_dim)
+        self.var_linear = nn.Linear(48, vae_dim)
+    
+    def encode(self, x):
+        gconditions = []
+        for i, encoder in enumerate(self.encoders):
+            gcondition = encoder(x)
+            x = self.pools[i-1](x)
+            gconditions.append(gcondition)
+        
+        outputs = torch.cat(gconditions, dim=-1)
+
+        mu = self.mean_linear(outputs)
+        var = self.var_linear(outputs)
+        return mu, var
+
+    def reparametrize(self, mu, log_var):
+        std = torch.exp(log_var * 0.5)
+        eps = torch.randn_like(std, requires_grad=True)
+        return mu + eps * std
+
+    def forward(self, x):
+        mu, var = self.encode(x)
+        z = self.reparametrize(mu, var) 
+        
+        return z, mu, var
+
+class EncoderNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.nets = nn.ModuleList([
+            nn.ReflectionPad1d(22),
+            weight_norm(Conv1d(1, 16, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(16, 32, stride=2, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(32, 64, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+            
+            nn.ReflectionPad1d(22),
+            weight_norm(Conv1d(64, 128, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(128, 256, stride=2, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(256, 256, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+            
+            nn.ReflectionPad1d(22),
+            weight_norm(Conv1d(256, 128, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(128, 64, stride=2, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(64, 32, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+
+            nn.ReflectionPad1d(22),
+            weight_norm(Conv1d(32, 16, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(16, 16, stride=2, kernel_size=10)),
+            nn.LeakyReLU(),
+            weight_norm(Conv1d(16, 16, stride=1, kernel_size=10)),
+            nn.LeakyReLU(),
+            ])
+
+        self.dense = nn.Linear(16, 16)
+
+    def forward(self, x):
+        for net in self.nets:
+            x = net(x)
+        x, _ = torch.max(x, dim=-1)
+        x = self.dense(x)
+
+        return x
+
 
